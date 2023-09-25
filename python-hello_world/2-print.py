121cc412af8d1d6b3d0359bf4f068b0fd0ea2883
MoeALAnnan <5653@holbertonstudents.com>
2023-09-25 08:23:34 +0000

    print function

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Write a Python script that prints exactly
+# "Programming is like building a multilingual puzzle, followed by a new line.
+# Use the function print
+# guillaume@ubuntu:~/py/$ ./2-print.py
+# "Programming is like building a multilingual puzzle
+# guillaume@ubuntu:~/py/$
+
+print("\"Programming is like building a multilingual puzzle\n")
