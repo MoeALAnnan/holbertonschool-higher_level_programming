83b2c7d0f61c03c4a3fd3f2869a9a97d82191cce
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-26 10:10:31 +0000

    adding .format(i)

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(99):
-    print(f"{i:02}, ", end="")
+    print(f"{i:02}, ".format(i), end="")
     if i == 98:
-        print(f"{i+1}")
+        print(f"{i+1}".format(i))
