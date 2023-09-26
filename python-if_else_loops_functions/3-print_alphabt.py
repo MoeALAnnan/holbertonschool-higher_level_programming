b080fc9a97d146b43d74cb4fa2157d3f115e0227
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-26 09:10:08 +0000

    removing if statement

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == ord('e') or i == ord('q'):
-        i += 1
-    print(f"{chr(i)}".format(i), end="")
+    if i != ord('e') and i != ord('q'):
+        print(f"{chr(i)}".format(i), end="")
