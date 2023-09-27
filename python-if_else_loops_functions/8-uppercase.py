90c363e233589e068adc8d809bada5d693f74b38
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-27 10:28:17 +0000

    print with string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def isupper(str):
     for i in str:
-        new_line = "\n"
         if ord(i) in range(97, 123):
             i = chr(ord(i) - 32)
-        print(f"{i}".format(i))
-    print(f"{new_line}".format(new_line))
+        print(f"{i}".format(i), end="")
+    print(f"".format())
