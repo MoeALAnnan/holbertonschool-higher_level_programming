fd074bd3bab8de0515237f2295e50c1406a8a8d6
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-29 18:53:34 +0000

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print(f"0 argument.")
+    elif len(sys.argv) == 2:
+        print(f"{len(sys.argv) - 1} argument:")
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: {sys.argv[i]}")
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+        for i in range(1, len(sys.argv)):
+            print(f"{i}: {sys.argv[i]}")
