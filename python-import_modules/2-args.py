9ce7eb7572cae2f37a264c05da42ee1daa3c3e0a
MoeAlAnnan <5653@holbertonstudents.com>
2023-09-29 19:00:10 +0000

    fixing typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == '__main__':
     if len(sys.argv) == 1 or sys.argv[0] is None:
-        print(f"0 argument.")
+        print(f"0 arguments.")
     elif len(sys.argv) == 2:
         print(f"{len(sys.argv) - 1} argument:")
         for i in range(1, len(sys.argv)):
