74c3afef2d90c77661a66818bd86b54ade2fc01c
MoeAlAnnan <5653@holbertonstudents.com>
2023-10-05 15:39:29 +0000

    removing the fstring in return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
     else:
-        return (f"{my_list[idx]}".format(my_list[idx]))
+        return (my_list[idx])
