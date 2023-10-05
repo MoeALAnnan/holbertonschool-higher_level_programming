de954bca2f8e1fa58929463a3f0ecd65b64372be
MoeAlAnnan <5653@holbertonstudents.com>
2023-10-05 15:15:42 +0000

    using string format and correcting typo

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return None
     else:
-        return (mylist[idx])
+        return (f"{my_list[idx]}".format(my_list[idx]))
