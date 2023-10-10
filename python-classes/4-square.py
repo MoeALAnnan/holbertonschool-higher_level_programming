6cecaca40a84196dccac863e39fb2b897004da0c
MoeAlAnnan <5653@holbertonstudents.com>
2023-10-10 08:46:29 +0000

    fixing pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,13 +10,14 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-        return(self.__size * self.__size)
-    
+        return (self.__size * self.__size)
+
     @property
     def size(self):
-        return(self.__size)
-    
+        return (self.__size)
+
     @size.setter
     def size(self, value):
         if not (isinstance(value, int)):
