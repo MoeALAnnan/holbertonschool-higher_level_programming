31465e4812b262756dd1bbc9223b613c56c0fccf
MoeAlAnnan <5653@holbertonstudents.com>
2023-10-27 17:14:13 +0000

    over all base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,3 +17,52 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """converting json object to string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return (json.dumps([]))
+        else:
+            return (json.dumps(list_dictionaries))
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of list_objs to a file """
+        filename = "{}.json".format(cls.__name__)
+
+        json_dict = []
+
+        with open(filename, "w", encoding="utf-8") as f:
+            if list_objs is None or list_objs == []:
+                f.write("[]")
+            else:
+                for obj in list_objs:
+                    dictionary = cls.to_dictionary(obj)
+                    json_dict.append(dictionary)
+                    dict_to_string = cls.to_json_string(json_dict)
+                f.write(dict_to_string)
+    @staticmethod
+    def from_json_string(json_string):
+        """return list"""
+        if json_string is not None:
+            return json.loads(json_string)
+        else:
+            return []
+
+    @classmethod
+    def create(cls, **dictionary):
+
+        dummy = cls(1, 1)
+        dummy.x = 0
+        dummy.y = 0
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        file = cls.__name__ + ".json"
+        try:
+            with open(file, 'r') as f:
+                return [cls.create(**dictionary) for
+                        dictionary in cls.from_json_string(f.read())]
+        except FileNotFoundError:
+            return []
