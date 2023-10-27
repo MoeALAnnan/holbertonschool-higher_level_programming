28f386198711fad1bbb0db9906a949c31312adb9
MoeAlAnnan <5653@holbertonstudents.com>
2023-10-27 17:17:26 +0000

    over all correction:wq

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
-""" base file """
+""" import json """
+import json
 
 
 class Base:
     """
-    This class will be the “base” of all other classes in this project.
-    The goal of it is to manage id attribute in all your future classes and
-    to avoid duplicating the same code (by extension, same bugs)
+    first class Base
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ init method"""
+        """ class constructor """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
-        """converting json object to string"""
+        """ json return the dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return (json.dumps([]))
+            return "[]"
         else:
-            return (json.dumps(list_dictionaries))
+            return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_file(cls, list_objs):
-        """ writes the JSON string representation of list_objs to a file """
-        filename = "{}.json".format(cls.__name__)
-
-        json_dict = []
-
-        with open(filename, "w", encoding="utf-8") as f:
-            if list_objs is None or list_objs == []:
-                f.write("[]")
+        """ json save"""
+        with open(cls.__name__+'.json', 'w+', encoding="utf-8") as file:
+            dict = []
+            if list_objs is not None and len(list_objs) != 0:
+                for i in list_objs:
+                    dict.append(cls.to_dictionary(i))
+                file.write(cls.to_json_string(dict))
             else:
-                for obj in list_objs:
-                    dictionary = cls.to_dictionary(obj)
-                    json_dict.append(dictionary)
-                    dict_to_string = cls.to_json_string(json_dict)
-                f.write(dict_to_string)
+                file.write("[]")
+
     @staticmethod
     def from_json_string(json_string):
         """return list"""
